fe2e3bce769fa8a96d6a9546f2a0c7ecccdbd7b0
박성진 <106302098+macboy5@users.noreply.github.com>
2024-07-07 23:04:56 +0900

    Update

--- app/src/main/java/fragments/CategoryNewsFragment.kt
+++ app/src/main/java/fragments/CategoryNewsFragment.kt
@@ -8,10 +8,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.unit_3.sogong_test.R
-import com.unit_3.sogong_test.databinding.FragmentChatBinding
+import com.unit_3.sogong_test.databinding.FragmentCategoryNewsBinding
 
 class CategoryNewsFragment : Fragment() {
-    private lateinit var binding : FragmentChatBinding
+    private lateinit var binding : FragmentCategoryNewsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +22,7 @@ class CategoryNewsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_category_news, container, false)
 
         binding.bottomNavigationHome.setOnClickListener {
             it.findNavController().navigate(R.id.action_chatFragment_to_homeFragment)
