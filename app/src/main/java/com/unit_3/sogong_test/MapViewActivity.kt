56df9d4e3a7d5fb1a1c40f5cc57b14827b97a74b
minjun011026 <jun011026@gmail.com>
2024-07-13 00:30:07 +0900

    위치 지정 및 저장

--- app/src/main/java/com/unit_3/sogong_test/MapViewActivity.kt
+++ app/src/main/java/com/unit_3/sogong_test/MapViewActivity.kt
@@ -1,17 +1,19 @@
 package com.unit_3.sogong_test
 
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.database.database
@@ -23,7 +25,11 @@ import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 import com.unit_3.sogong_test.databinding.ActivityMapViewBinding
+import jxl.Workbook
+import jxl.read.biff.BiffException
+import java.io.IOException
 import java.util.Locale
+
 
 class MapViewActivity : AppCompatActivity() , OnMapReadyCallback {
     private val LOCATION_PERMISSION_REQUEST_CODE = 5000
@@ -32,12 +38,16 @@ class MapViewActivity : AppCompatActivity() , OnMapReadyCallback {
         android.Manifest.permission.ACCESS_FINE_LOCATION,
         android.Manifest.permission.ACCESS_COARSE_LOCATION
     )
-
+    val nearCity = ArrayList<String>()
     private lateinit var binding: ActivityMapViewBinding
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
-    private lateinit var locationName:TextView
+    private lateinit var listViewAdapter : ListViewAdapter
+    var addressName : String = ""
     private val marker = Marker()
+    private var curlatitude = 0.0
+    private var curlongitude = 0.0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,20 +57,36 @@ class MapViewActivity : AppCompatActivity() , OnMapReadyCallback {
         } else {
             initMapView()
         }
+        val addressBtn1 = findViewById<Button>(R.id.addressBtn1)
+        val bottomSheetDialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
+        val bottomSheetView = LayoutInflater.from(applicationContext).inflate(R.layout.layout_bottom_sheet, null)
+        listViewAdapter = ListViewAdapter(this, nearCity, bottomSheetDialog)
 
-        val locationSaveBtn = findViewById<Button>(R.id.locationSaveBtn)
-
-        locationSaveBtn.setOnClickListener{
-            if(locationName.text.toString() != "지역명"){
-                val database = Firebase.database
-                val myRef = database.getReference("location").child(Firebase.auth.currentUser!!.uid)
-                myRef.push().setValue(locationName.text.toString())
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+        addressBtn1.setOnClickListener{
+            if(addressName != ""){
+//                val database = Firebase.database
+//                val myRef = database.getReference("location").child(Firebase.auth.currentUser!!.uid)
+//                myRef.push().setValue(addressName)
+                readExcel(addressName)
+                bottomSheetView.findViewById<ListView>(R.id.listView).adapter = listViewAdapter
+                bottomSheetDialog.setContentView(bottomSheetView)
+                bottomSheetDialog.show()
+                Log.d("herere", nearCity.joinToString(" "))
+                addressName = ""
             }
         }
 
-        locationName = findViewById<TextView>(R.id.locationName)
+        val addressBtn2 = findViewById<Button>(R.id.addressBtn2)
+
+        addressBtn2.setOnClickListener{
+            if(addressName != ""){
+                val database = Firebase.database
+                val myRef = database.getReference("location").child(Firebase.auth.currentUser!!.uid)
+                addressBtn2.text = addressName
+                myRef.push().setValue(addressName)
+                addressName = ""
+            }
+        }
     }
 
     private fun initMapView() {
@@ -89,6 +115,7 @@ class MapViewActivity : AppCompatActivity() , OnMapReadyCallback {
 
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
+
         // 현재 위치
         naverMap.locationSource = locationSource
         // 현재 위치 버튼 기능
@@ -111,7 +138,7 @@ class MapViewActivity : AppCompatActivity() , OnMapReadyCallback {
     private fun getAddress(latitude: Double, longitude: Double) {
         // Geocoder 선언
         val geocoder = Geocoder(applicationContext, Locale.KOREAN)
-
+        Log.d("checheckchcheck", "getAddress occur")
         // 안드로이드 API 레벨이 33 이상인 경우
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             geocoder.getFromLocation(
@@ -121,13 +148,14 @@ class MapViewActivity : AppCompatActivity() , OnMapReadyCallback {
                     // 반환 값에서 전체 주소만 사용한다.
                     // getAddressLine(0)
                     toast(address[0].locality)
-                    locationName.text=address[0].locality
+                    addressName=address[0].locality
                 }
             }
         } else { // API 레벨이 33 미만인 경우
             val addresses = geocoder.getFromLocation(latitude, longitude, 1)
             if (addresses != null) {
                 toast(addresses[0].locality)
+                addressName=addresses[0].locality
             }
         }
     }
@@ -135,6 +163,43 @@ class MapViewActivity : AppCompatActivity() , OnMapReadyCallback {
     private fun toast(text: String) {
         runOnUiThread {
             Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun readExcel(localName: String?) {
+        try {
+            val `is` = baseContext.resources.assets.open("coordinate.xls")
+            val wb = Workbook.getWorkbook(`is`)
+
+            if (wb != null) {
+                val sheet = wb.getSheet(2)
+                if (sheet != null) {
+                    val colTotal = sheet.columns
+                    val rowIndexStart = 1
+                    val rowTotal = sheet.rows-1
+                    Log.d("rowTotal", "$rowTotal")
+
+                    var row = rowIndexStart
+                    while (row < rowTotal) {
+                        val contents = sheet.getCell(1, row).contents
+                        if (contents.contains(localName!!)) {
+                            val x = sheet.getCell(5, row).contents
+                            val y = sheet.getCell(6, row).contents
+                            if(sheet.getCell(2, row).contents.isNotEmpty()) {
+                                nearCity.add(sheet.getCell(2, row).contents)
+                            }
+                        }
+                        row++
+                    }
+                    listViewAdapter.notifyDataSetChanged()
+                }
+            }
+        } catch (e: IOException) {
+            Log.i("READ_EXCEL1", e.message!!)
+            e.printStackTrace()
+        } catch (e: BiffException) {
+            Log.i("READ_EXCEL1", e.message!!)
+            e.printStackTrace()
         }
     }
 }
