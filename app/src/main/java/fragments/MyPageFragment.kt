4876bba3b98519fde9dc67c3d21518ad69c46b66
Codingbaek <a01048070210@gmail.com>
2024-07-26 17:02:43 +0900

    Update

--- app/src/main/java/fragments/MyPageFragment.kt
+++ app/src/main/java/fragments/MyPageFragment.kt
@@ -3,22 +3,27 @@ package fragments
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.unit_3.sogong_test.*
 import com.unit_3.sogong_test.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
     private lateinit var binding: FragmentMyPageBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false)
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
 
         // Bottom navigation click listeners
         binding.bottomNavigationLocal.setOnClickListener {
@@ -57,8 +62,33 @@ class MyPageFragment : Fragment() {
 
         // 비밀번호 변경 버튼 클릭 리스너 추가
         binding.buttonChangePassword.setOnClickListener {
-            val intent = Intent(requireContext(),  VerifyCurrentPasswordActivity::class.java)
+            val intent = Intent(requireContext(), VerifyCurrentPasswordActivity::class.java)
             startActivity(intent)
+        }
+
+        // 로그아웃 버튼 클릭 리스너 추가
+        binding.buttonLogout.setOnClickListener {
+            firebaseAuth.signOut()
+            // Navigate to login activity or main activity
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()  // Close MyPageActivity
+        }
+
+        // 회원 탈퇴 버튼 클릭 리스너 추가
+        binding.buttonAccountDeletion.setOnClickListener {
+            val user = firebaseAuth.currentUser
+            user?.delete()?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // User account deleted
+                    val intent = Intent(requireContext(), LoginActivity::class.java)
+                    startActivity(intent)
+                    requireActivity().finish()  // Close MyPageActivity
+                } else {
+                    // Handle failure
+                    // You can use Toast or other UI elements to notify the user
+                }
+            }
         }
 
         // SharedPreferences에서 닉네임과 이메일 불러오기
