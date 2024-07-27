7ecad4e2ccb652016c0a83bc837286098e5bde31
박성진 <106302098+macboy5@users.noreply.github.com>
2024-07-28 00:25:18 +0900

    트렌드 페이지 업데이트

--- app/src/main/java/com/unit_3/sogong_test/TrendRVAdapter.kt
+++ app/src/main/java/com/unit_3/sogong_test/TrendRVAdapter.kt
@@ -7,10 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.database.database
 
 class TrendRVAdapter(private val keywords: MutableList<TrendKeywordsModel>) :
     RecyclerView.Adapter<TrendRVAdapter.ViewHolder>() {
@@ -20,6 +24,8 @@ class TrendRVAdapter(private val keywords: MutableList<TrendKeywordsModel>) :
         val keywordTextView: TextView = view.findViewById(R.id.keywordTextView)
         val searchCountTextView: TextView = view.findViewById(R.id.searchCountTextView)
         val imageArea: ImageView = view.findViewById(R.id.imageArea)
+        val addKeywordBtn : ImageView = view.findViewById(R.id.addKeywordBtn)
+        val trendItem : LinearLayout = view.findViewById(R.id.trendItem)
 
         fun bind(keyword: TrendKeywordsModel, context: Context, position: Int) {
             Log.d("TrendRVAdapter", "bind: $keyword")
@@ -31,11 +37,21 @@ class TrendRVAdapter(private val keywords: MutableList<TrendKeywordsModel>) :
                 .load(keyword.imageUrl)
                 .into(imageArea)
 
-            itemView.setOnClickListener {
+            trendItem.setOnClickListener {
                 Toast.makeText(context, "키워드 클릭: ${keyword}", Toast.LENGTH_LONG).show()
                 val intent = Intent(context, TrendKeywordActivity::class.java)
                 intent.putExtra("keyword_title", keyword.keyword)  // 필요한 필드만을 전달
                 context.startActivity(intent)
+            }
+
+            addKeywordBtn.setOnClickListener {
+                val keyword = keyword.keyword
+
+                if(keyword != null){
+                    val database = Firebase.database
+                    val myRef = database.getReference("keyword").child(Firebase.auth.currentUser!!.uid)
+                    myRef.push().setValue(KeywordModel(keyword.toString()))
+                }
             }
         }
     }
