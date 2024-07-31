13eb366a2bd3fe9c74d62dbaf9c48159c2ef5a6f
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-07-31 22:58:13 +0900

    로딩중화면구현
    이쁘게 꾸미는건 이제 해야함

--- app/src/main/java/com/unit_3/sogong_test/SplashActivity.kt
+++ app/src/main/java/com/unit_3/sogong_test/SplashActivity.kt
@@ -4,22 +4,47 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
 
-class SplashActivity : AppCompatActivity() {
+//class SplashActivity : AppCompatActivity() {
+//    private lateinit var animationView: LottieAnimationView
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_splash)
+//
+//        // LottieAnimationView 초기화
+//        animationView = findViewById(R.id.lottieAnimationView)
+//
+//        // 애니메이션이 끝난 후 다음 액티비티로 이동
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 2000) // 2초 후에 다음 액티비티로 이동
+//    }
+//}
+
+ class SplashActivity : AppCompatActivity() {
+     private lateinit var animationView: LottieAnimationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
         setContentView(R.layout.activity_splash)
+        try {
+            animationView = findViewById(R.id.lottieAnimationView)
+        } catch (e: Exception) {
+            e.printStackTrace() // 로그 출력
+        }
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 4000)
 
     }
 }
